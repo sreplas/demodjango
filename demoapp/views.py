c7d83040a65700ce3360329a0fe893d5886ab327
sreplas <61392290+sreplas@users.noreply.github.com>
2023-01-08 14:06:22 +0300

    Update views.py

--- demoapp/views.py
+++ demoapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {"data":"Home Page of Django App"}
+    context = {"data":"Welcome to SELCO SCORDS"}
     return render(request,'demoapp/index.html', context)
